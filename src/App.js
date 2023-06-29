a45a0c8bba2835cfa4ad58a2e4a7b8dd3ef073d6
alinebsv <115201122+alinebsv@users.noreply.github.com>
2023-06-28 21:48:39 -0700

    Made it mobile responsive

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
       <h1>Fitness Helper</h1>
       <h2>Calculate your protein and calorie intake</h2>
       <div className="row">
-        <div className="col-6 mt-4 intakeCalculator">
+        <div className="col-sm-6 mt-4 intakeCalculator">
           <h3>Calculate your ideal <span className="highlight">protein</span> intake</h3>
           <ProteinIntake />
           </div>
-        <div className="col-6 mt-4 intakeCalculator">
+        <div className="col-sm-6 mt-4 intakeCalculator">
         <h3>Calculate your ideal <span className="highlight">calorie</span> intake</h3>
 
           <CalorieIntake />
@@ -25,7 +25,7 @@ function App() {
           </div>
     </div>
     <div className="row">
-      <div className="col text-center mt-5 weightConverter d-flex justify-content-center">
+      <div className="col-sm text-center mt-5 weightConverter d-flex justify-content-center">
         
         <WeightConverter />
       </div>
